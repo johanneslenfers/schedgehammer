dd5f1594665ce6c75822fcb57d006f4456faa00a
Linkiablo <johajansen@googlemail.com>
2024-10-12 18:05:20 +0200

    formatting

--- kick_off_assignment/tuner/parameter.py
+++ kick_off_assignment/tuner/parameter.py
@@ -2,37 +2,44 @@ from dataclasses import dataclass
 from random import shuffle
 import random
 
+
 @dataclass
 class Parameter:
     pass
 
+
 @dataclass
 class SwitchParameter(Parameter):
     pass
+
 
 @dataclass
 class RealParameter(Parameter):
     min: float
     max: float
 
+
 @dataclass
 class IntegerParameter(Parameter):
     min: int
     max: int
 
+
 @dataclass
 class OrdinalParameter(Parameter):
     vals: list[any]
+
 
 @dataclass
 class CategoricalParameter(Parameter):
     vals: list[str]
 
+
 @dataclass
 class PermutationParameter(Parameter):
     vals: list[int]
-    
+
+
 @dataclass
 class Config:
     parameters: dict[str, Parameter]
-
--- kick_off_assignment/tuner/tuner.py
+++ kick_off_assignment/tuner/tuner.py
@@ -1,35 +1,47 @@
 from dataclasses import dataclass
 from typing import Callable
+import random
 
-from tuner.parameter import *
+from tuner.parameter import Config, SwitchParameter, RealParameter, IntegerParameter, OrdinalParameter, CategoricalParameter, PermutationParameter
+
 
 @dataclass
 class Tuner:
     config: Config
-    budget: int = 50
+    budget: int = 100
 
     def get_dict(self) -> dict[str, bool | float | int | str | list[int]]:
         ret = {}
 
         for [name, param] in self.config.parameters.items():
             match param:
-                case SwitchParameter(): ret[name] = True
-                case RealParameter(min, max): ret[name] = min + random.random() * (max - min)
-                case IntegerParameter(min, max): ret[name] = random.randint(min, max)
-                case OrdinalParameter(vals): ret[name] = vals[random.randint(0, len(vals) - 1)]
-                case CategoricalParameter(vals): ret[name] = vals[random.randint(0, len(vals) - 1)]
-                case PermutationParameter(vals): tmp = vals.copy() ;shuffle(tmp) ;ret[name] = tmp
+                case SwitchParameter():
+                    ret[name] = True
+                case RealParameter(min, max):
+                    ret[name] = min + random.random() * (max - min)
+                case IntegerParameter(min, max):
+                    ret[name] = random.randint(min, max)
+                case OrdinalParameter(vals):
+                    ret[name] = vals[random.randint(0, len(vals) - 1)]
+                case CategoricalParameter(vals):
+                    ret[name] = vals[random.randint(0, len(vals) - 1)]
+                case PermutationParameter(vals):
+                    tmp = vals.copy()
+                    random.shuffle(tmp)
+                    ret[name] = tmp
 
         return ret
 
     def tune(self, cost_fn: Callable[[dict[str, any]], float]) -> float:
-        min = cost_fn(self.get_dict())
+        min_dict = self.get_dict()
+        min = cost_fn(min_dict)
         for _ in range(self.budget):
-            cur = cost_fn(self.get_dict())
+            cur_dict = self.get_dict()
+            cur = cost_fn(cur_dict)
 
             min = cur if cur < min else min
+            min_dict = cur_dict if cur < min else min_dict
 
         print(f"minimal cost: {min}")
+        print(f"minimal dict: {min_dict}")
         return min
-
-            
