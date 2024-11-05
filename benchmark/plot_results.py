edb06c3a9eae818f1dab2b8b576cdc5b21eb9adc
Justus Dieckmann <justusdieckmann@uni-muenster.de>
2024-11-05 13:20:24 +0100

    Draw results into seperate plots

--- benchmark/plot_results.py
+++ benchmark/plot_results.py
@@ -11,12 +11,16 @@ def plot_results():
     for benchmark_name, scores in results.items():
         if benchmark_name == "total_time_schedgehammer":
             continue
+        plt.figure()
         plt.plot(scores, label=benchmark_name)
-    plt.plot(results["total_time_schedgehammer"], label="total_time")
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.legend()
-    plt.show()
+        plt.xscale("log")
+        plt.xlabel("function evaluations")
+        plt.ylabel("cost")
+        plt.title(benchmark_name)
+        plt.savefig(f"results/{benchmark_name}.png")
+    # plt.plot(results["total_time_schedgehammer"], label="total_time")
+    # plt.legend()
+    # plt.show()
 
 
 if __name__ == "__main__":
