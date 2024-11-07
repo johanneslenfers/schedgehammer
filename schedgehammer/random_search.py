4a37bc7ace0caa087f7a7e075c177a46f8da1a04
Justus Dieckmann <justusdieckmann@gmail.com>
2024-11-07 04:07:47 +0100

    Add random search

---
+++ schedgehammer/random_search.py
@@ -0,0 +1,11 @@
+from schedgehammer.tuner import Tuner, TuningAttempt
+
+
+class RandomSearch(Tuner):
+
+    def do_tuning(self, tuning_attempt: TuningAttempt):
+        while tuning_attempt.in_budget():
+            config = {}
+            for [name, param] in tuning_attempt.problem.params.items():
+                config[name] = param.choose_random()
+            tuning_attempt.evaluate_config(config)
