6935f1048480a54f854d882d883a11200b23e006
Linkiablo <johajansen@googlemail.com>
2024-11-15 18:27:00 +0100

    generic constraints

--- schedgehammer/constraints.py
+++ schedgehammer/constraints.py
@@ -1,62 +1,45 @@
 import copy
 import random
 from dataclasses import dataclass
-from typing import Callable, Iterable
+from typing import Callable, Iterable, Optional
+from param_types import ParamValue
 
 
 @dataclass
 class Constraint:
-    pass
-
-
-@dataclass
-class LessThan(Constraint):
     var1: str
     var2: str
+    fn: Callable
 
-    def filter(self, variables: dict[str, list[any]]):
-        new_domain1 = list(
-            filter(lambda x: x < max(variables[self.var2]), variables[self.var1])
-        )
-        new_domain2 = list(
-            filter(lambda x: x > min(variables[self.var1]), variables[self.var2])
-        )
+    def filter(self, variables: dict[str, list[any]]) -> Optional[bool]:
+        """
+        Filters variable domains.
+        Returns True if changed, False if no valid values remain,
+        None if nothing changed
+        """
+        size = len(variables[self.var1]) + len(variables[self.var2])
 
-        if len(new_domain1) == 0 or len(new_domain2) == 0:
+        new_domain2 = set()
+        for i in variables[self.var1]:
+            new_domain2 = new_domain2.union(
+                filter(lambda x: self.fn(i, x), variables[self.var2])
+            )
+
+        if len(new_domain2) == 0:
             return False
 
-        variables[self.var1] = new_domain1
-        variables[self.var2] = new_domain2
+        variables[self.var2] = list(new_domain2)
 
-        if len(new_domain1) < len(variables[self.var1]) or len(
-            variables[self.var2]
-        ) < len(new_domain2):
-            return True
-
-        return None
-
-
-@dataclass
-class NotEqual(Constraint):
-    var1: str
-    var2: str
-
-    def filter(self, variables: dict[str, list[any]]):
-        size = len(variables[self.var1]) + len(variables[self.var2])
-        if len(variables[self.var1]) == 1:
-            new_domain2 = list(
-                filter(lambda x: x != variables[self.var1][0], variables[self.var2])
+        new_domain1 = set()
+        for i in variables[self.var2]:
+            new_domain1 = new_domain1.union(
+                filter(lambda x: self.fn(x, i), variables[self.var1])
             )
-            if len(new_domain2) == 0:
-                return False
-            variables[self.var2] = new_domain2
-        if len(variables[self.var2]) == 1:
-            new_domain1 = list(
-                filter(lambda x: x != variables[self.var2][0], variables[self.var1])
-            )
-            if len(new_domain1) == 0:
-                return False
-            variables[self.var1] = new_domain1
+
+        if len(new_domain1) == 0:
+            return False
+
+        variables[self.var1] = list(new_domain1)
 
         if size > len(variables[self.var1]) + len(variables[self.var2]):
             return True
@@ -64,13 +47,20 @@ class NotEqual(Constraint):
             return None
 
 
+Variables = dict[str, list[ParamValue]]
+
+
 @dataclass
 class Solver:
     variables: dict[str, list[any]]
     constraints: list[Constraint]
-    exploration_function: Callable[[Iterable], any] = random.choice
+    exploration_function: Callable[[Iterable], ParamValue] = random.choice
 
-    def make_decision(self, variables):
+    def make_decision(self, variables: Variables) -> tuple[str, ParamValue]:
+        """
+        Fixes a parameter to a value.
+        Value is chosen based on exploration function
+        """
         var, domain = next(
             filter(lambda x: len(x[1]) > 1, variables.items()), (None, None)
         )
@@ -80,7 +70,10 @@ class Solver:
         else:
             return None, None
 
-    def fix_point(self, variables):
+    def fix_point(self, variables: Variables) -> bool:
+        """
+        Returns True, if applying any constraint does not change the search space
+        """
         while True:
             fltrs = False
             for c in self.constraints:
@@ -92,12 +85,21 @@ class Solver:
             if not fltrs:
                 return True
 
-    def apply_decision(self, variables, var, val, apply):
+    def apply_decision(
+        self, variables: Variables, var: str, val: ParamValue, apply: bool
+    ):
+        """
+        If apply is True, fix parameter to single value, else remove
+        value from search space
+        """
         c = copy.deepcopy(variables)
         c[var] = list(filter(lambda x: apply == (x == val), c[var]))
         yield from self.dfs(c)
 
-    def dfs(self, variables):
+    def dfs(self, variables: Variables):
+        """
+        Generate valid configurations, using dfs
+        """
         if self.fix_point(variables):
             var, val = self.make_decision(variables)
             if var is None:
@@ -112,9 +114,8 @@ class Solver:
 
 if __name__ == "__main__":
     variables = {"a": [1, 2, 3], "b": [1, 2, 3]}
-    ne = NotEqual("a", "b")
-    lt = LessThan("a", "b")
-    solver = Solver(variables, [lt], min)
+    g = Constraint("a", "b", lambda x, y: x == y)
+    solver = Solver(variables, [g], random.choice)
     solutions = list(solver.solve())
     print(f"there are {len(solutions)} solutions")
     print(solutions)
