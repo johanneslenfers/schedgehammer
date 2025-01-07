553f6beb0654256033c8c1ddab73496b6c67081b
Sven Spehr <spehr.sv@gmail.com>
2025-01-06 18:21:48 -0800

    feat: plot confidence interval for ansor

--- examples/schedule_test.py
+++ examples/schedule_test.py
@@ -27,14 +27,26 @@ results = []
 ansor_results = []
 
 
+def plot_results_from_several_runs(results, label) -> range:
+    """Plot results from multiple runs and returns x values"""
+    zipped_results = list(zip(*results))
+    means = [np.mean(x) for x in zipped_results]
+    mins = [np.min(x) for x in zipped_results]
+    maxs = [np.max(x) for x in zipped_results]
+    xs = range(len(zipped_results))
+    plt.plot(xs, means, label=label)
+    plt.fill_between(xs, mins, maxs, alpha=0.3)
+    return xs
+
+
 class StoreResultCallback(PythonBasedMeasureCallback):
     def callback(self, policy, inputs, results):
-        ansor_results.append(float(results[0].costs[0]))
-        for result in results[1:]:
+        for result in results[0:]:
             cost = float(result.costs[0])
-            ansor_results.append(
-                cost if cost < ansor_results[-1] else ansor_results[-1]
-            )
+            if not ansor_results[-1] or cost < ansor_results[-1][-1]:
+                ansor_results[-1].append(cost)
+            else:
+                ansor_results[-1].append(ansor_results[-1][-1])
 
 
 def create_schedule() -> ScheduleEnvironment:
@@ -97,41 +109,43 @@ def get_ansor_results():
 
     # Create the search task
     target = tvm.target.Target("llvm")
-    task = auto_scheduler.SearchTask(func=create_task_func, target=target)
+    for _ in range(RUNS):
+        ansor_results.append([])
+        task = auto_scheduler.SearchTask(func=create_task_func, target=target)
 
-    tuning_options = auto_scheduler.TuningOptions(
-        num_measure_trials=ITERATIONS,
-        measure_callbacks=[
-            auto_scheduler.RecordToFile("matmul.json"),
-            StoreResultCallback(),
-        ],
-        verbose=2,
-    )
+        tuning_options = auto_scheduler.TuningOptions(
+            num_measure_trials=ITERATIONS,
+            measure_callbacks=[
+                auto_scheduler.RecordToFile("matmul.json"),
+                StoreResultCallback(),
+            ],
+            verbose=2,
+        )
 
-    # Begin tuning process
-    task.tune(tuning_options)
+        # Begin tuning process
+        task.tune(tuning_options)
 
-    # Apply the best schedule and build the function
-    sch, args = task.apply_best("matmul.json")
-    func = tvm.build(sch, args, target)
+        # Apply the best schedule and build the function
+        sch, args = task.apply_best("matmul.json")
+        func = tvm.build(sch, args, target)
 
-    dev = tvm.device("llvm", 0)
-    # Create sample input arrays
-    a_np = np.random.uniform(size=(M, K)).astype(DTYPE)
-    b_np = np.random.uniform(size=(K, N)).astype(DTYPE)
-    c_np = np.zeros((M, N), dtype=DTYPE)
+        dev = tvm.device("llvm", 0)
+        # Create sample input arrays
+        a_np = np.random.uniform(size=(M, K)).astype(DTYPE)
+        b_np = np.random.uniform(size=(K, N)).astype(DTYPE)
+        c_np = np.zeros((M, N), dtype=DTYPE)
 
-    # Create TVM NDArray
-    a_tvm = tvm.nd.array(a_np)
-    b_tvm = tvm.nd.array(b_np)
-    c_tvm = tvm.nd.array(c_np)
+        # Create TVM NDArray
+        a_tvm = tvm.nd.array(a_np)
+        b_tvm = tvm.nd.array(b_np)
+        c_tvm = tvm.nd.array(c_np)
 
-    evaluator = func.time_evaluator(func.entry_name, dev, number=5)
-    exec_time = evaluator(a_tvm, b_tvm, c_tvm).mean
-    print("Ansor execution time: %.3f ms" % (exec_time * 1e3))
-    ansor_results[-1] = (
-        exec_time  # Make sure the real execution time is not higher than calculated by ansor
-    )
+        evaluator = func.time_evaluator(func.entry_name, dev, number=5)
+        exec_time = evaluator(a_tvm, b_tvm, c_tvm).mean
+        print("Ansor execution time: %.3f ms" % (exec_time * 1e3))
+        ansor_results[-1][-1] = (
+            exec_time  # Make sure the real execution time is not higher than calculated by ansor
+        )
 
 
 def get_baseline() -> float:
@@ -200,15 +214,9 @@ if __name__ == "__main__":
                 best_block_schedule, get_blocking_baseline(2**bn_exp, 2**kfactor_exp)
             )
 
-    zipped_results = list(zip(*results))
-    means = [np.mean(x) for x in zipped_results]
-    mins = [np.min(x) for x in zipped_results]
-    maxs = [np.max(x) for x in zipped_results]
-    xs = range(len(zipped_results))
     plt.figure()
-    plt.plot(xs, means, label="Random Search")
-    plt.plot(range(len(ansor_results)), ansor_results, label="Ansor Baseline")
-    plt.fill_between(xs, mins, maxs, alpha=0.3)
+    plot_results_from_several_runs(results, "Random Search")
+    xs = plot_results_from_several_runs(ansor_results, "Ansor")
     plt.plot(xs, [baseline_score] * len(xs), label="Baseline")
     plt.plot(
         xs,
